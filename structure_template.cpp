2238a9c2fa3370a1d017d8bc749bb5aae01f0977
Palak234 <gaurithapliyal43@gmail.com>
2024-11-11 02:46:35 +0530

    Add files via upload

---
+++ structure_template.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+template<class T1,class T2,class T3,class T4>
+struct Student{
+    T1 name;
+    T2 age;
+    T3 course;
+    T4 department;
+    Student(T1 n,T2 a,T3 c,T4 d){
+        name=n;
+        age=a;
+        course=c;
+        department=d;
+    }
+    void display(){
+        cout<<"Name:"<<name<<endl;
+        cout<<"Age:"<<age<<endl;
+        cout<<"Course:"<<course<<endl;
+        cout<<"Department:"<<department<<endl;
+    }
+};
+int main(){
+    Student<string,int,string,string> obj("Palak",19,"C++","Computer Science");
+    obj.display();
+    return 0;
+}
